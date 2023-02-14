861d9489a9dfdb7f235984bf499b61d212308cf1
Hayato <hayato.314.nico@gmail.com>
2023-02-15 04:19:19 +0900

    チャクラUI導入とボタンの調整

--- ieal/src/components/Hero.jsx
+++ ieal/src/components/Hero.jsx
@@ -3,7 +3,8 @@ import { Wife } from '../pages/wife';
 import { Husband } from '../pages/husband';
 import { BrowserRouter, Routes, Route, Link,  } from "react-router-dom";
 import  styled  from "styled-components";
-
+// import { Button } from '@mui/material';
+import { Button, ButtonGroup, ChakraProvider } from '@chakra-ui/react';
 
 export const Hero = () =>{
     return(
@@ -14,13 +15,14 @@ export const Hero = () =>{
         </HeroStyle>
             <BrowserRouter>
             <LinkStyle>
-                <SelectStyle>
-                    <Link to="/husband">Husband</Link>
-                </SelectStyle>
-                <br />
-                <SelectStyle>
-                    <Link to="/wife">Wife</Link>
-                </SelectStyle>
+                <ChakraProvider>
+                    <Button colorScheme='blue'>
+                        <Link to="/husband">Husband</Link>
+                    </Button>
+                    <Button colorScheme='pink'>
+                        <Link to="/wife">Wife</Link>
+                    </Button>
+                </ChakraProvider>
             </LinkStyle>
     
 
@@ -39,24 +41,28 @@ const HeroStyle = styled.div`
 `;
 
 const TitleStyle = styled.h1`
-  color: #333d51;
+  color: #fff;
   text-align: center;
   font-size:50px;
   margin:0px;
 `;
 
 const MesseageStyle = styled.p`
-  color: #333d51;
+  color: #ffff;
   text-align: center;
   font-size:28px;
   margin:0px;
   `;
 
-  const LinkStyle = styled.div`
+const LinkStyle = styled.div`
     display:flex;
     justify-content: space-around;
   `;
 
-  const SelectStyle = styled.div`
+const SelectStyle = styled.div`
     width:500px;
+  `;
+
+const ButtonStyle = styled.div`
+    margin
   `;
